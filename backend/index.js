4b16864c6db41c0b68ea8a544491aeaf5fc7080e
Rohan Darji <rohandarji2001@gmail.com>
2024-02-08 00:31:34 -0500

    nodemon, Initial routes

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,16 @@ const express = require('express');
 
 // Create an instance of Express
 const app = express();
+const workoutRoutes = require('./routes/workouts')
+
+// Middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 // Define routes
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+app.use(workoutRoutes)
 
 // Start the server
 const port = 3000;
