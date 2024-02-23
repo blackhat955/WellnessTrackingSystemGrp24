f7ca6b2b75710e0c870a9d09f8d104918fae009a
jkkatari <jkkatari@iu.edu>
2024-02-23 14:28:10 -0500

    funtionality update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,39 +1,16 @@
-// // const express = require('express');
-// // const router = express.Router();
-// // const cors = require('cors');
-// // const { test, registerUser } = require('../controllers/authControllers')
+const express = require('express');
+const router = express.Router();
+const cors = require('cors');
+const { test, registerUser } = require('../controllers/authControllers')
 
-// // router.use(
-// //     cors({
-// //         credentials: true,
-// //         origin: 'http://localhost:5173'
-// //     })
-// // )
+router.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:5173'
+    })
+)
 
-// // router.get('/', test)
-// // router.post('/register', registerUser)
+router.get('/', test) 
+router.post('/register', registerUser)
 
-// // module.exports = router    
-
-// const express = require('express');
-// const router = express.Router();
-// const cors = require('cors');
-// const { test, registerUser } = require('../controllers/authControllers')
-
-// router.use(
-//     cors({
-//         credentials: true,
-//         origin: 'http://localhost:5173'
-//     })
-// )
-
-// router.get('/', test)
-// router.post('/register', registerUser)
-
-// // Error handling middleware
-// router.use((err, req, res, next) => {
-//     console.error(err.stack);
-//     res.status(500).send('Something went wrong!');
-// });
-
-// module.exports = router;
+module.exports = router;
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,25 +1,25 @@
-// const express = require('express');
+const express = require('express');
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.get('/', (req, res) => {
-//     res.send('Hello, world!');
-// })
+router.get('/', (req, res) => {
+    res.send('Hello, world!');
+})
 
-// router.get('/about', (req, res) => {
-//     res.send('About page');
-// })
+router.get('/about', (req, res) => {
+    res.send('About page');
+})
 
-// router.get('/profile', (req, res) => {
-//     res.send('User Profile Page');
-// })
+router.get('/profile', (req, res) => {
+    res.send('User Profile Page');
+})
 
-// router.get('/register', (req, res) => {
-//     res.send('User Registration Page');
-// })
+router.get('/register', (req, res) => {
+    res.send('User Registration Page');
+})
 
-// router.get('/login', (req, res) => {
-//     res.send('User Login Page');
-// })
+router.get('/login', (req, res) => {
+    res.send('User Login Page');
+})
 
-// module.exports = router
+module.exports = router
