3c230039cf0406a741885eaea5c1a1d9eb9d4905
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2024-02-26 14:46:05 -0500

    add register page, login page, professional page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import RegistrationPage from './pages/registrationPages/resPage';
+import Findprofessional from './pages/findProfessional';
+import LoginPage from './pages/loginPages/logPage';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <LoginPage/> 
+    
     </div>
   );
 }
